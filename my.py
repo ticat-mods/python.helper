422b2823dcbe0ff04aa7054f8a6feedf4c8bc75f
Liu Cong <innerr@gmail.com>
2022-02-11 04:54:18 +0800

    handle errors in mysql_execute
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- my.py
+++ my.py
@@ -1,4 +1,5 @@
-import os
+import subprocess
+import sys
 
 # Use os executing to avoid the dependency of mysql.connector
 def my_exe(host, port, user, db, query, fmt):
@@ -11,9 +12,19 @@ def my_exe(host, port, user, db, query, fmt):
 			't': '--table',
 		}[fmt]
 	query = ' '.join(query.split())
-	cmd_line='mysql -h "%s" -P "%s" -u "%s" --database="%s" --comments %s -e "%s"' % (host, port, user, db, fmt, query)
 
-	lines = os.popen(cmd_line).readlines()
+	args = ['mysql', '-h', host, '-P', port, '-u', user, '--database=%s' % db, '--comments', fmt, '-e', query]
+
+	if sys.version_info.major == 2:
+		cmd = subprocess.Popen(args, stdout=subprocess.PIPE)
+	else:
+		cmd = subprocess.Popen(args, stdout=subprocess.PIPE, encoding='utf8')
+	cmd.wait()
+	if cmd.returncode != 0:
+		sys.stderr.write(cmd.stdout.read())
+		raise Exception('os.wait: exit status != 0')
+
+	lines = cmd.stdout.readlines()
 	if len(lines) > 0:
 		lines = lines[1:]
 
@@ -21,5 +32,11 @@ def my_exe(host, port, user, db, query, fmt):
 	for line in lines:
 		if len(line) != 0:
 			line = line[:-1]
-		result.append(line.split('\t'))
+		if sys.version_info.major == 2:
+			line = line.encode('utf8')
+		cols = line.split('\t')
+		if len(cols) == 1:
+			result.append(cols[0])
+		else:
+			result.append(cols)
 	return result
