b102b597f35221c6b2a8759611d207ce509080af
Liu Cong <innerr@gmail.com>
2022-02-12 11:19:58 +0800

    add to_true|to_false
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- strs.py
+++ strs.py
@@ -4,3 +4,9 @@ def colorize(code, origin):
 	if code <= 0 or code >= 256:
 		return origin
 	return '\033[38;5;' + str(code) + 'm' + origin + '\033[0m'
+
+def to_true(s):
+	return s.lower() in ['true', 't', 'yes', 'y', 'on', '1']
+
+def to_false(s):
+	return s.lower() in ['false', 'f', 'no', 'n', 'off', '0']
